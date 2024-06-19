f8c0762255657a52f1e8186858221544a1e576a6
Alex <32714651+aharral09@users.noreply.github.com>
2024-06-19 03:34:20 +0000

    removed

--- nvim/lua/aharral/plugins/bufferline.lua
+++
@@ -1,8 +0,0 @@
-local bufferline = require('bufferline')
-    bufferline.setup ({
-        options = {
-            mode = "buffers", -- set to "tabs" to only show tabpages instead
-            style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
-        },
-    })
-
