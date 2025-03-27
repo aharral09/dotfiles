6d0b8175d92f927b9a6b17b40976c8d21b9ab0f3
Alex Harral <aharral@wistar.org>
2025-03-27 10:27:22 -0400

    updates

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,15 +1,16 @@
-local wezterm = require("wezterm")
+local weztem = require("wezterm")
 local config = wezterm.config_builder()
 
+config.default_prog = { 'zsh' }
 config.color_scheme = 'Vs Code Dark+ (Gogh)'
-config.font_size = 14
+config.font_size = 11
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.enable_tab_bar = true
 config.window_decorations = "TITLE | RESIZE"
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.97
 config.adjust_window_size_when_changing_font_size = false
 config.colors = {
   cursor_bg = '#ffffff',
@@ -28,6 +29,10 @@ config.keys = {
     mods = 'ALT',
     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
+  {
+    key = 'k',
+    mods = 'ALT',
+    action = wezterm.action.SendString 'tmux-sessionizer\n'
+  },
 }
-config.default_prog = { 'zsh' }
 return config
