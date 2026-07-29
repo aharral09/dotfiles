155025d7359de4dcc1bc400f750a760facec5165
Alex Harral <aharral@wa-gx1qwwkjtp.wistar.upenn.edu>
2026-07-29 11:01:21 -0400

    cleanup

--- new-install.sh
+++
@@ -1,3 +0,0 @@
-sudo dnf -y update && sudo dnf -y upgrade
-
-sudo dnf -y install git curl wget firefox ripgrep tmux alacritty
--- zOld/packer/.config/nvim/lua/aharral/plugins/neogit.lua
+++
@@ -1,9 +0,0 @@
--- import lualine plugin safely
-local status, neogit = pcall(require, "neogit")
-if not status then
-  return
-end
-
-
--- configure lualine with modified theme
-neogit.setup{}
